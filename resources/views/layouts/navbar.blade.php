fdab9e91d505d66416ce7a12f8e9a59a6e43a562
baguscs <cahyob59@gmail.com>
2023-12-23 04:11:19 +0700

    fix bug on menu toogle

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,10 +1,53 @@
 {{-- <x-splade-toogle> --}}
     <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
-        <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-            <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-                <i class="bx bx-menu bx-sm"></i>
-            </a>
+        <div class=" me-3 me-xl-0 d-xl-none">
+
+            <div class="btn-group" id="dropdown-icon-demo">
+                <button class="btn btn-primary dropdown-toggle hide-arrow" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bx bx-menu"></i>
+                </button>
+                <ul class="dropdown-menu">
+                    <Link href="{{ route('dashboard') }}" class="app-brand-link ml-5 mt-3">
+                        <x-application-logo></x-application-logo>
+                    </Link>
+                    <li class="ml-5 mt-3 {{ request()->routeIs('dashboard') ? 'active' : '' }}">
+                        <Link as="button" href="{{ route('dashboard') }}" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-home-circle"></i> Beranda
+                        </Link>
+                    </li>
+                    <li class="menu-header small text-uppercase mt-3">
+                        <span class="menu-header-text fw-bold ml-5" style="font-size: 12px">masyarakat</span>
+                    </li>
+                    <li class="ml-5 mt-3 {{ request()->routeIs('villager.index') ? 'active' : '' }}">
+                        <Link as="button" href="{{ route('villager.index') }}" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-group"></i> Warga
+                        </Link>
+                    </li>
+                    <li class="ml-5 mt-3 {{ request()->routeIs('operator.index') ? 'active' : '' }}">
+                        <Link as="button" href="{{ route('operator.index') }}" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-shield-quarter"></i>
+                            <div>Pengurus</div>
+                        </Link>
+                    </li>
+                    <li class="menu-header small text-uppercase mt-3">
+                        <span class="menu-header-text fw-bold ml-5" style="font-size: 12px">pelayanan</span>
+                    </li>
+                    <li class="menu-item ml-5 mt-3">
+                        <a href="S" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-box"></i>
+                            <div>Pengajuan</div>
+                        </a>
+                    </li>
+                    <li class="menu-item ml-5 mt-3">
+                        <a href="S" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-list-check"></i>
+                            <div>Validasi Pengajuan</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
+
 
         <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
 
@@ -63,13 +106,6 @@
                             <div class="dropdown-divider"></div>
                         </li>
                         <li>
-                            {{-- <form action="{{ route('logout') }}" method="POST">
-                            @csrf
-                                <Link class="dropdown-item" type="submit">
-                                    <i class="bx bx-power-off me-2"></i>
-                                    <span class="align-middle">Log Out</span>
-                                </Link>
-                            </form> --}}
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
