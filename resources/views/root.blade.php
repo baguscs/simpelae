31662487c5a0384f6357f1433f98f3cd73f53a6c
baguscs <cahyob59@gmail.com>
2024-03-23 15:35:14 +0700

    remove unused plugins

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -39,7 +39,6 @@
         @spladeHead
     </head>
     <body class="font-sans antialiased">
-        @include('sweetalert::alert')
         @splade
     </body>
 
@@ -51,14 +50,11 @@
       "resources/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js",
       "resources/assets/vendor/js/menu.js",
       "resources/assets/js/main.js",
-    //   "resources/assets/vendor/libs/apex-charts/apexcharts.js",
-    //   "resources/assets/js/dashboards-analytics.js",
       "public/sw.js"
     ])
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
 
     {{-- PWA --}}
     @stack('js')
